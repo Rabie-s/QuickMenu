56d654cdf9cb8851ea60387ad47cfe0d4cfd457c
Rabie-s <asdfw.jsd@gmail.com>
2025-05-01 14:41:45 +0300

    u

--- back-end/start/routes.ts
+++ back-end/start/routes.ts
@@ -4,6 +4,9 @@ import userAuthApiRoutes from './routes/v1/userAuthApi.js'
 import menusApiRoutes from './routes/v1/menusApi.js'
 import categoriesApiRoutes from './routes/v1/categoriesApi.js'
 import menuItemsApiRoutes from './routes/v1/menuItemsApi.js'
+
+import publicMenusApiRoutes from './routes/v1/public/publicMenusApi.js'
+
 router.group(() => {
 
     userAuthApiRoutes(router)
@@ -14,8 +17,10 @@ router.group(() => {
         menuItemsApiRoutes(router)
     }).use(middleware.auth())
 
-}).prefix('user/v1')
+    
 
+}).prefix('user/v1')
+publicMenusApiRoutes(router)
 
 
 
